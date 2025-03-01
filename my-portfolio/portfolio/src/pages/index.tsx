818a61d505a498128b0a973a2d3aed858f8a3bb8
isulnethila <139636196+isulnethila@users.noreply.github.com>
2025-03-02 01:29:40 +0530

    bulid service i offerd part

--- my-portfolio/portfolio/src/pages/index.tsx
+++ my-portfolio/portfolio/src/pages/index.tsx
@@ -5,8 +5,15 @@ import {BsFillMoonStarsFill } from "react-icons/bs";
 import { FaGithub } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa6";
+import profileImage from "../assets/q.jpg";
+import graphicDesining from "../assets/design.png";
+import code from "../assets/code.png";
+import web from "../assets/web.png";
+import web1 from "../assets/web1.png"
+import web2 from "../assets/web2.png"
+import web3 from "../assets/web4.png"
+import web4 from "../assets/web5.png"
 
-import profileImage from "../assets/q.jpg"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -64,6 +71,56 @@ Currently, I am building projects that enhance my skills in mobile and web devel
             objectFit="cover"
           />
 
+        </div>
+      </section>
+      {/* services */}
+      <section>
+        <h2 className="text-5xl opacity-65 font-bold py-10">Service I offer</h2>
+        <p className="text-sm text-gray-400 leading-6"> offer professional mobile app and website development services, specializing in React Native for cross-platform apps and responsive web solutions. My expertise includes UI/UX implementation, API integration, performance optimization, and bug fixing to ensure smooth functionality. Whether you need a new app, a dynamic website, or enhancements to an existing project, I am committed to delivering high-quality solutions tailored to your needs. Let's build something great together! </p>
+
+        {/* services cards */}
+        <div>
+          {/* card1 */}
+          <div className="bg-yellow-300 text-center shadow-sm p-5 rounded-xl my-10">
+              <Image
+                src={graphicDesining}
+                alt="Graphic Desinging"
+                width={100}
+                height={100}
+                className=" inline"
+              />
+              <h2 className="text-2xl font-bold">Graphic Designing</h2>
+              <p className="text-sm text-gray-700 py-2 leading-6">I create visually appealing designs that enhance brand identity and user experience. My services include logo design, social media graphics, UI/UX elements, and marketing materials. With a focus on creativity and detail, I bring ideas to life through clean and engaging designs.</p>
+          </div>
+          <div className="bg-yellow-300 text-center shadow-sm p-5 rounded-xl my-10">
+              <Image
+                src={code}
+                alt="Graphic Desinging"
+                width={100}
+                height={100}
+                className=" inline"
+              />
+              <h2 className="text-2xl font-bold">Application development</h2>
+              <p className="text-sm text-gray-700 py-2 leading-6">I develop high-quality mobile and web applications tailored to your needs. Specializing in React Native and modern web technologies, I create user-friendly, efficient, and scalable applications with seamless functionality and intuitive design.</p>
+          </div>
+          <div className="bg-yellow-300 text-center shadow-sm p-5 rounded-xl my-10">
+              <Image
+                src={web}
+                alt="Graphic Desinging"
+                width={100}
+                height={100}
+                className=" inline"
+              />
+              <h2 className="text-2xl font-bold">web development</h2>
+              <p className="text-sm text-gray-700 py-2 leading-6">I build responsive and dynamic websites tailored to your needs. Using modern web technologies, I create fast, user-friendly, and visually appealing websites that provide a seamless experience across all devices.</p>
+          </div>
+        </div>
+      </section>
+      {/* my work */}
+      <section>
+        <div>
+          <h2>Potfolio</h2>
+          <p className="text-sm text-gray-400 leading-6">I showcase my work through high-quality visuals, including app screenshots, website designs, and graphic samples. From intuitive mobile apps to responsive websites and eye-catching graphics, my portfolio reflects my skills in creating functional and visually appealing digital solutions.</p>
         </div>
       </section>
     </main>
