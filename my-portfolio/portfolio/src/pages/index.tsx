1c154e9968ca0a901560197b9699ec9bd369c48f
isulnethila <139636196+isulnethila@users.noreply.github.com>
2025-03-02 18:18:30 +0530

    update link

--- my-portfolio/portfolio/src/pages/index.tsx
+++ my-portfolio/portfolio/src/pages/index.tsx
@@ -47,7 +47,7 @@ const [darkmode, setdarkmode] = useState(false);
                 className="cursor-pointer text-xl mx-5 "/>
             </li>
             <li>
-              <a href="https://drive.google.com/file/d/1UY3xkiO2Z6nUKQu5L2jpMobMuzHm3_ff/view?usp=sharing" 
+              <a href="https://drive.google.com/file/d/14PDUic-JxgvDiHHaiBM4uZcjcwh08DSo/view?usp=drive_link" 
               className=" bg-yellow-400 py-2 px-5 text-black border-none rounded-2xl "
               download={"resume"}>Resume</a>
             </li>
